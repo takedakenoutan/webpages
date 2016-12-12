e8ea49168ce8c6cb4c0e467d6aee3a5a783fa15f
takedakenoutan <takedajojo@yahoo.co.jp>
2016-12-12 19:05:42 +0900

    安定版

--- docs/src/temp.js
+++ docs/src/temp.js
@@ -6,5 +6,9 @@ var writing ={
 	navi : function(){
 		let html = '<h3 id= "menutext">めにゅー</h3><ul id= "menu"><li id= "menu01"><a href="index.html">トップページ</a></li></ul>';
 		document.getElementById("navi").innerHTML = html;
+	},
+	address : function(){
+		let html = '<div class="g-person" data-width="200" data-href="//plus.google.com/u/0/115038182927590902152" data-rel="author"></div>';
+		document.getElementById("address").innerHTML = html;
 	}
 };
