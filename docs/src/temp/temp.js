c212d764cb725455bafcfc4cace6dd01fc3feb7a
takedakenoutan <takedajojo@yahoo.co.jp>
2016-12-21 21:04:50 +0900

    改行すんのわすれてたぜ。てへぺろ

--- docs/src/temp/temp.js
+++ docs/src/temp/temp.js
@@ -9,7 +9,7 @@ var writing ={
 		html += '<ul id= "menu">';
 			html += '<li id= "menu01"><a href= "https://takedakenoutan.github.io/webpages/index.html">トップページ</a></li><br>';
 			html += '<li id= "menu02"><a href= "https://takedakenoutan.github.io/webpages/sites/test-sites/">特に意味のないページ</a></li><br/>';
-			html += '<li id= "menu03"><a href= "https://takedakenoutan.github.io/webpages/sites/info-sites/">自己紹介とか</a></li>';
+			html += '<li id= "menu03"><a href= "https://takedakenoutan.github.io/webpages/sites/info-sites/">自己紹介とか</a></li><br>';
 			html += '<li id= "menu04"><a href= "https://takedakenoutan.github.io/webpages/sites/admin-sites/">管理ページ</a></li>';
 		html += '</ul>';
 		document.getElementById("navi").innerHTML = html;
