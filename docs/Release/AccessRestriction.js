b0073f29e7b4db8d1fa0046d30cfe3349748c1a7
takedakenoutan <takedajojo@yahoo.co.jp>
2016-12-22 14:55:52 +0900

    微修正

--- docs/Release/AccessRestriction.js
+++ docs/Release/AccessRestriction.js
@@ -1,7 +1,7 @@
 (
 	function()
 	{
-		let AccessR = document.getElementByTagName("AccessR");
+		let AccessR = document.getElementsByTagName("AccessR");
 		for(var i = 0;i < AccessR[i].length;i++)
 		{
 			if(AccessR[i].attributes("id"))
