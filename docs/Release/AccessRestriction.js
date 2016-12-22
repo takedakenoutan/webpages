a2bd54ceef58d989c0456da45c91b5e86bfa3f98
takedakenoutan <takedajojo@yahoo.co.jp>
2016-12-22 14:53:10 +0900

    AccessRestriction仮実装したよーん

---
+++ docs/Release/AccessRestriction.js
@@ -0,0 +1,18 @@
+(
+	function()
+	{
+		let AccessR = document.getElementByTagName("AccessR");
+		for(var i = 0;i < AccessR[i].length;i++)
+		{
+			if(AccessR[i].attributes("id"))
+			{
+				var id = AccessR[i];
+			}
+			if(AccessR[i].attributes("pass"))
+			{
+				var pass = AccessR[i];
+			}
+		}
+		window.confirm("Id : " + id + "\nPass : " + pass);
+	}
+)();
