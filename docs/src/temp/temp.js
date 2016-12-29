0344f0151b53febdc5767359017ef09b9c0328a1
takedakenoutan <takedajojo@yahoo.co.jp>
2016-12-29 22:57:39 +0900

    変数名をhtmlからtextに

--- docs/src/temp/temp.js
+++ docs/src/temp/temp.js
@@ -1,22 +1,22 @@
 var writing ={
 	header : function(){
-		let html = '<h1 id= "h-title"><a href= "https://takedakenoutan.github.io/webpages/index.html">武田家のほむぺ</a></h1>';
-		document.getElementById("header").innerHTML = html;
+		let text = '<h1 id= "h-title"><a href= "https://takedakenoutan.github.io/webpages/index.html">武田家のほむぺ</a></h1>';
+		document.getElementById("header").innerHTML = text;
 	},
 	navi : function(){
-		let html = '';
-		html += '<h3 id= "menutext">めにゅー</h3>';
-		html += '<ul id= "menu">';
-			html += '<li id= "menu01"><a href= "https://takedakenoutan.github.io/webpages/index.html">トップページ</a></li><br>';
-			html += '<li id= "menu02"><a href= "https://takedakenoutan.github.io/webpages/sites/test-sites/">特に意味のないページ</a></li><br/>';
-			html += '<li id= "menu03"><a href= "https://takedakenoutan.github.io/webpages/sites/info-sites/">自己紹介とか</a></li><br>';
-			html += '<li id= "menu04"><a href= "https://takedakenoutan.github.io/webpages/sites/admin-sites/">管理ページ</a></li><br>';
-			html += '<li id= "menu05"><a href= "https://takedakenoutan.github.io/webpages/sites/AccessRestriction-sites/">AccessRistriction</a></li>';
-		html += '</ul>';
-		document.getElementById("navi").innerHTML = html;
+		let text = '';
+		text += '<h3 id= "menutext">めにゅー</h3>';
+		text += '<ul id= "menu">';
+			text += '<li id= "menu01"><a href= "https://takedakenoutan.github.io/webpages/index.html">トップページ</a></li><br>';
+			text += '<li id= "menu02"><a href= "https://takedakenoutan.github.io/webpages/sites/test-sites/">特に意味のないページ</a></li><br/>';
+			text += '<li id= "menu03"><a href= "https://takedakenoutan.github.io/webpages/sites/info-sites/">自己紹介とか</a></li><br>';
+			text += '<li id= "menu04"><a href= "https://takedakenoutan.github.io/webpages/sites/admin-sites/">管理ページ</a></li><br>';
+			text += '<li id= "menu05"><a href= "https://takedakenoutan.github.io/webpages/sites/AccessRestriction-sites/">AccessRistriction</a></li>';
+		text += '</ul>';
+		document.getElementById("navi").innerHTML = text;
 	},
 	address : function(){
-		let html = '<div class="g-person" data-width="200" data-href="//plus.google.com/u/0/115038182927590902152" data-rel="author"></div>';
-		document.getElementById("address").innerHTML = html;
+		let text = '<div class="g-person" data-width="200" data-href="//plus.google.com/u/0/115038182927590902152" data-rel="author"></div>';
+		document.getElementById("address").innerHTML = text;
 	}
 };
