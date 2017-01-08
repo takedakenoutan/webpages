d037a582cb222460c84424178626572ce187f476
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-08 21:55:48 +0900

    一旦てすと

---
+++ sites/URLShortener-sites/src/script.js
@@ -0,0 +1,8 @@
+function dia(){
+	var result=window.prompt("アクセストークンを入力してください");
+	if(result!=null){
+		window.prompt("コピーしてください",'javascript:(function(key){var xhr=new XMLHttpRequest();xhr.open("GET","https://api-ssl.bitly.com/v3/shorten?access_token="+key+"&longUrl="+location.href,false);xhr.send(null);var res=JSON.parse(xhr.response)["data"]["url"];window.prompt("URLShortener",res);})('+result+')');
+	}else{
+		alert("アクセストークンが入力されていません");
+	}
+}
