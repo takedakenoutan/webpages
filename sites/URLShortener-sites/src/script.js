14f66e94eff8fe8a4b26b063c1bbbd23de7f5e7f
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-08 22:13:27 +0900

    完成したかな？

--- sites/URLShortener-sites/src/script.js
+++ sites/URLShortener-sites/src/script.js
@@ -1,6 +1,6 @@
 function dia(){
 	var result=window.prompt("アクセストークンを入力してください");
-	if(result!=null){
+	if(result!=null&&result!=""){
 		window.prompt("コピーしてください",'javascript:(function(key){var xhr=new XMLHttpRequest();xhr.open("GET","https://api-ssl.bitly.com/v3/shorten?access_token="+key+"&longUrl="+location.href,false);xhr.send(null);var res=JSON.parse(xhr.response)["data"]["url"];window.prompt("URLShortener",res);})(\"'+result+'\");');
 	}else{
 		alert("アクセストークンが入力されていません");
