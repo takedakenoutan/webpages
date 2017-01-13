f1a9527c6cbb33c04beb1a449e2e9b946ba03bbf
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-13 19:31:22 +0900

    scriptをつくってみた

---
+++ src/script/closed.js
@@ -0,0 +1,9 @@
+(function(){
+	var xhr = new XMLHttpRequest;
+	xhr.open("https://takedakenoutan.github.io/webpages/src/config.json",false);
+	xhr.send(null);
+	var res = JSON.parse(xhr.response)["closed"];
+	if(res){
+		location.href = "https://takedakenoutan.github.io/webpages/Closed.html";
+	}
+})();
