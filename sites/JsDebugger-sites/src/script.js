0567a9d7e760d1d714f3f3722c864d1ab1fb86b4
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-21 18:51:50 +0900

    実行するコードを表示

--- sites/JsDebugger-sites/src/script.js
+++ sites/JsDebugger-sites/src/script.js
@@ -6,15 +6,14 @@ function debug(){
 function main(){
 	var text = debug();
 	var nText = text.replace(/<br>/g, "\n");
-	var cText = "try{" + nText + "}catch(er){errorEvent(er);}";
+	var cText = 'try{' + nText + '}catch(er){errorEvent(er);}';
 	var scr = document.createElement("script");
-	document.getElementById("display").innerHTML = text;
+	document.getElementById("display").innerHTML = cText;
 	scr.innerHTML = nText;
 	document.getElementById("scr").appendChild(scr);
 	document.getElementById("error").innerHTML = None;
 }
-
 function errorEvent(er){
-	var erText = '<font color= "red">ErrorType:' + er.name + '<br>ErrorMessage:' + er.message + "</font>";
-	document.getElementById("error").innerHTML = erText;
+var erText = '<font color= "red">ErrorType:' + er.name + '<br>ErrorMessage:' + er.message + '</font>';
+document.getElementById("error").innerHTML = erText;
 }
