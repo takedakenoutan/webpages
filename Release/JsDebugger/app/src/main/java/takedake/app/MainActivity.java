9a66c9f3df8cf7f3da23e05f6a31a959b4b60d40
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-22 08:11:07 +0900

    update

--- Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
+++ Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
@@ -2,8 +2,7 @@ package takedake.app;
 
 import android.app.*;
 import android.os.*;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
 
 public class MainActivity extends Activity 
 {
@@ -13,12 +12,6 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-        WebView  myWebView = (WebView)findViewById(R.id.JsWeb);
 
-        myWebView.setWebViewClient(new WebViewClient());
-
-        myWebView.loadUrl("https://takedakenoutan.github.io/webpages/sites/JsDebugger-sites/");
-
-        myWebView.getSettings().setJavaScriptEnabled(true);
     }
 }
