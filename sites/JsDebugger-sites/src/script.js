2aae16d71edf68b75cb7af989a8ec37ee8203a06
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-21 19:03:30 +0900

    凡ミス

--- sites/JsDebugger-sites/src/script.js
+++ sites/JsDebugger-sites/src/script.js
@@ -8,12 +8,8 @@ function main(){
 	var nText = text.replace(/<br>/g, "\n");
 	var cText = 'try{' + nText + '}catch(er){errorEvent(er);}';
 	var scr = document.createElement("script");
-	document.getElementById("display").innerHTML = cText;
-	scr.innerHTML = nText;
+	document.getElementById("display").innerHTML = text;
+	scr.innerHTML = cText;
 	document.getElementById("scr").appendChild(scr);
 	document.getElementById("error").innerHTML = None;
-}
-function errorEvent(er){
-var erText = '<font color= "red">ErrorType:' + er.name + '<br>ErrorMessage:' + er.message + '</font>';
-document.getElementById("error").innerHTML = erText;
 }
