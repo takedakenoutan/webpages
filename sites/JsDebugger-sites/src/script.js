c2fdd82ea713c8a1ce89a6ab7243600b8fa8dd1c
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-21 11:58:28 +0900

    赤文字

--- sites/JsDebugger-sites/src/script.js
+++ sites/JsDebugger-sites/src/script.js
@@ -6,10 +6,11 @@ function debug(){
 function main(){
 	var text = debug();
 	document.getElementById("display").innerHTML = text;
+	document.getElementById("error").innerHTML = None;
 	eval("try{" + text + "}catch(er){errorEvent(er);}");
 }
 
 function errorEvent(er){
-	var erText = "ErrorType:" + er.name + "\nErrorMessage:" + er.message;
+	var erText = '<font color= "red">ErrorType:' + er.name + '<br>ErrorMessage:' + er.message + "</font>";
 	document.getElementById("error").innerHTML = erText;
 }
