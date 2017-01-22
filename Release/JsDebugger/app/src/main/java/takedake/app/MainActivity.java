79deb4bf3ea824f300bb9aca3bee7e4149455b28
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-22 20:26:38 +0900

    あとはcustomtabsのカスタムだけさ

--- Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
+++ Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
@@ -2,7 +2,9 @@ package takedake.app;
 
 import android.app.*;
 import android.os.*;
-
+import android.net.Uri;
+import android.support.customtabs.*;
+import org.chromium.customtabsclient.shared.CustomTabsHelper;
 
 public class MainActivity extends Activity 
 {
@@ -12,6 +14,11 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-
+		String url = "https://takedakenoutan.github.io/webpages/sites/JsDebugger-sites/";
+		String packageName = CustomTabsHelper.getPackageNameToUse(this);
+		CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+		CustomTabsIntent customTabsIntent = builder.build();
+		customTabsIntent.intent.setPackage(packageName);
+		customTabsIntent.launchUrl(this, Uri.parse(url));
     }
 }
