ec3c56c088328daa24f797ad8b08120b3d42e99d
takedakenoutan <takedajojo@yahoo.co.jp>
2017-01-25 22:07:05 +0900

    はいはい、あぷであぷで

--- Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
+++ Release/JsDebugger/app/src/main/java/takedake/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.*;
 import android.net.Uri;
 import android.support.customtabs.*;
 import org.chromium.customtabsclient.shared.CustomTabsHelper;
+import android.content.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
 {
@@ -14,11 +16,24 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
+		createDebug cDebug = new createDebug();
+		cDebug.openDebug(this);
+    }
+	public void cButton(View view){
+		createDebug cDebug = new createDebug();
+		cDebug.openDebug(this);
+	}
+}
+
+class createDebug{
+	public void openDebug(Activity self){
 		String url = "https://takedakenoutan.github.io/webpages/sites/JsDebugger-sites/";
-		String packageName = CustomTabsHelper.getPackageNameToUse(this);
+		String packageName = CustomTabsHelper.getPackageNameToUse(self);
 		CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+		builder.setShowTitle(true);
+
 		CustomTabsIntent customTabsIntent = builder.build();
 		customTabsIntent.intent.setPackage(packageName);
-		customTabsIntent.launchUrl(this, Uri.parse(url));
-    }
+		customTabsIntent.launchUrl(self, Uri.parse(url));
+	}
 }
