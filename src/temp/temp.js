bfa9758a91e7c481a76d7c91dbe54a61a56b3816
takedakenoutan <takedajojo@yahoo.co.jp>
2017-03-15 19:56:15 +0900

    Gistの解説のっけた

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -7,7 +7,7 @@ function navi(){
 	text += '<h3 id= "menutext">めにゅー</h3>';
 	text += '<ul id= "menu">';
 		text += '<li id= "menu01"><a href= "https://takedakenoutan.github.io/webpages/index.html">トップページ</a></li><br>';
-		text += '<li id= "menu02"><a href= "https://takedakenoutan.github.io/webpages/sites/test-sites/">特に意味の...</a></li><br/>';
+		text += '<li id= "menu02"><a href= "https://takedakenoutan.github.io/webpages/sites/Gists-sites/">Gistの解説...</a></li><br/>';
 		text += '<li id= "menu03"><a href= "https://takedakenoutan.github.io/webpages/sites/info-sites/">自己紹介とか</a></li><br>';
 		text += '<li id= "menu04"><a href= "https://takedakenoutan.github.io/webpages/sites/admin-sites/">管理ページ</a></li><br>';
 		text += '<li id= "menu05"><a href= "https://takedakenoutan.github.io/webpages/sites/AccessRestriction-sites/">AccessRestriction</a></li><br>';
